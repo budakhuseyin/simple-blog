ccfc5c30057ccbdf1acb6d29d945519739a229f1
budakhuseyin <huseyinbudak904@gmail.com>
2026-02-10 17:02:07 +0300

    e-mail change ip and port

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -26,12 +26,11 @@ router.post('/', async (req, res) => {
 
             const transporter = nodemailer.createTransport({
                 host: 'smtp.gmail.com',
-                port: 587,
-                secure: false,
-                requireTLS: true,
+                port: 465,
+                secure: true, // SSL
                 logger: true,
                 debug: true,
-                family: 4, // ⚠️ Force IPv4 (Fixes ENETUNREACH on IPv6)
+                family: 4, // ⚠️ Force IPv4
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
